ea7c2fb447058e0ccaa8514d3c131400cc615532
Rafał Toborek <kontakt@toborek.info>
2022-03-17 14:32:54 +0100

    autodetect input file format

--- project/src/Command/ConvertCommand.php
+++ project/src/Command/ConvertCommand.php
@@ -18,11 +18,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ConvertCommand extends Command
 {
     const SUPPORTED_INPUT_FORMATS = [
-        'sword' => 'eSword',
-        'zefania' => 'Zefania XML',
-        'word' => 'theWord',
-        'tsv' => 'TSV (tab as a separator)',
-        'mybible' => 'MyBible',
+        'sword' => [
+            'name' => 'eSword',
+            'ext' => ['bblx', 'mybible'],
+        ],
+        'zefania' => [
+            'name' => 'Zefania XML',
+            'ext' => ['xml'],
+        ],
+        'word' => [
+            'name' => 'theWord',
+            'ext' => ['ot', 'nt', 'ont'],
+        ],
+        'tsv' => [
+            'name' => 'TSV (tab as a separator)',
+            'ext' => ['tsv'],
+        ],
+        'mybible' => [
+            'name' => 'MyBible',
+            'ext' => ['sqlite3'],
+        ],
     ];
 
     /** @var string */
@@ -38,43 +53,49 @@ class ConvertCommand extends Command
             ->setDescription('Converts given translation file to XML format')
             ->setHelp('Convert Bible translation in one of the supported formats to XML file used by rBiblia.')
             ->addArgument('input', InputArgument::REQUIRED, 'Input translation file (eg. translation.bblx)')
-            ->addArgument('output', InputArgument::OPTIONAL, 'Output XML file (eg. translation.xml)')
-            ->addArgument('format', InputArgument::OPTIONAL, 'Source file format', 'sword')
+            ->addArgument('output', InputArgument::OPTIONAL, 'Output XML file (eg. trans.xml, if not given then will be auto-generated)')
+            ->addArgument('format', InputArgument::OPTIONAL, 'Source file format (if not given then will be auto-detected)')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $source = $input->getArgument('input');
+        $sourceRealPath = realpath($source);
         $this->output = $output;
 
-        $format = $input->getArgument('format');
-        $source = realpath($input->getArgument('input'));
+        // input file not found
+        if (false === $sourceRealPath || !file_exists($sourceRealPath)) {
+            return $this->displayError(sprintf('File `%s` not found', $source));
+        }
 
         // generate output filename
         $destination = $input->getArgument('output');
         if (empty($destination)) {
-            $destination = preg_replace('/\.[^.]+$/', '.xml', $source);
+            $destination = preg_replace('/\.[^.]+$/', '.xml', $sourceRealPath);
         }
 
-        // input file not found
-        if (!file_exists($source)) {
-            return $this->displayError(sprintf('File `%s` not found', $source));
+        $format = $input->getArgument('format');
+
+        if (empty($format)) {
+            $format = $this->getFormatBasedOnExtension($sourceRealPath);
         }
+
+        $inputConverterClass = sprintf('rBibliaBibleConverter\Input\%sInput', ucfirst($format));
 
         // selected input converter not supported
-        $inputConverterClass = sprintf('rBibliaBibleConverter\Input\%sInput', ucfirst($format));
         if (!class_exists($inputConverterClass)) {
             return $this->displayError(sprintf('Input file format is not supported, supported formats are: <info>%s</info>',
                 implode(', ', array_keys(self::SUPPORTED_INPUT_FORMATS))));
         }
 
         $output->writeln([
-            sprintf('Converting: <info>%s</info>', $source),
+            sprintf('Converting: <info>%s</info>', $sourceRealPath),
             sprintf('Output: <info>%s</info>', $destination),
-            sprintf('Format: <info>%s</info>', self::SUPPORTED_INPUT_FORMATS[$format]),
+            sprintf('Format: <info>%s</info>', self::SUPPORTED_INPUT_FORMATS[$format]['name']),
         ]);
 
-        $file = new File($source);
+        $file = new File($sourceRealPath);
 
         $this->output->writeln([
             '',
@@ -102,6 +123,19 @@ class ConvertCommand extends Command
         ]);
 
         return Command::SUCCESS;
+    }
+
+    private function getFormatBasedOnExtension(string $filename): ?string
+    {
+        $extension = strtolower(pathinfo($filename, \PATHINFO_EXTENSION));
+
+        foreach (self::SUPPORTED_INPUT_FORMATS as $format => $settings) {
+            if (\in_array($extension, $settings['ext'], true)) {
+                return $format;
+            }
+        }
+
+        return null;
     }
 
     private function displayError(string $message): int
